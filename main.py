0ba7204eb7bbe13f56879b56c7553ca9a202f7db
Philipnah <Philip@han.dk>
2020-09-11 14:55:40 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,6 @@
+# Ophav. (år/seneste opdatering). Titel(udgave). Lokaliseret [besøgsdato] på: URL
+# 
+# Eksempel:
+# Holten, N. (2008, 20. februar). Etik. Lokaliseret d. 23. november 2009 på: http://fysio.dk/fafo/Etik/
+
+
