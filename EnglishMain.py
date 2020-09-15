dfc271a789d81616e65c58156c44b55c3e072973
Philipnah <Philip@han.dk>
2020-09-15 12:55:18 +0200

    Fixed formatting

--- EnglishMain.py
+++ EnglishMain.py
@@ -1,6 +1,6 @@
 
 websiteName = input("Website name: ")
-yearAndLastUpdate = input("Year & date of latest update: ")
+yearAndLastUpdate = input("('Year' 'date of latest update'): ")
 title = input("Title: ")
 visitDate = input("Date when website was retreived: ")
 url = input("URL: ")
@@ -8,6 +8,6 @@ url = input("URL: ")
 year = yearAndLastUpdate[:4]
 yearAndLastUpdate = yearAndLastUpdate[5:]
 
-output = websiteName + ". " + "(" + year + ", " + yearAndLastUpdate + "). " + title + ". Retrieved " + visitDate + "at: " + url
+output = websiteName + ". " + "(" + year + ", " + yearAndLastUpdate + "). " + title + ". Retrieved " + visitDate + " at: " + url
 
 print("\n\n" + output + "\n")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 websiteName = input("Hjemmesidens navn: ")
-yearAndLastUpdate = input("År og dato for sidste opdatering af hjemmesiden: ")
+yearAndLastUpdate = input("('År' 'dato for sidste opdatering af hjemmesiden'): ")
 title = input("Titel: ")
 visitDate = input("Hjemmeside besøgt den: ")
 url = input("URL: ")
@@ -8,6 +8,6 @@ url = input("URL: ")
 year = yearAndLastUpdate[:4]
 yearAndLastUpdate = yearAndLastUpdate[5:]
 
-output = websiteName + ". " + "(" + year + ", " + yearAndLastUpdate + "). " + title + ". Lokaliseret " + visitDate + "på: " + url
+output = websiteName + ". " + "(" + year + ", " + yearAndLastUpdate + "). " + title + ". Lokaliseret " + visitDate + " på: " + url
 
 print("\n\n" + output + "\n")
